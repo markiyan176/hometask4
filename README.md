a712c7d44bb835f91cbc093fd7e939f4fd3bac6c
markiyan176 <goro100100@gmail.com>
2025-12-10 03:01:11 +0200

    hometask4

---
+++ README.md
@@ -0,0 +1,8 @@
+Ferendovych Markiyan 4CS-31
+
+```
+./run.sh - запуск інстанса
+ssh -i key-pair.pem ubuntu@<ip> - підключення до інстансу
+su - adminuser - увійти в користувача adminuser
+su - poweruser - увійти в користувача poweruser
+```
---
+++ data.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+apt update -y
+
+useradd -m -s /bin/bash adminuser
+echo "adminuser:123" | chpasswd
+usermod -aG sudo adminuser
+
+useradd -m -s /bin/bash poweruser
+passwd -d poweruser
+
+echo "poweruser ALL=(ALL) NOPASSWD: /usr/sbin/iptables" > /etc/sudoers.d/poweruser
+chmod 440 /etc/sudoers.d/poweruser
+
+sed -i 's/^#*PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sed -i 's/^#*KbdInteractiveAuthentication.*/KbdInteractiveAuthentication yes/' /etc/ssh/sshd_config
+sed -i 's/^PermitEmptyPasswords.*/PermitEmptyPasswords no/' /etc/ssh/sshd_config
+
+systemctl restart sshd
+
+chmod 750 /home/adminuser
+chgrp poweruser /home/adminuser
+
+ln -s /etc/mtab /home/poweruser/mtab-link
+chown poweruser:poweruser /home/poweruser/mtab-link
